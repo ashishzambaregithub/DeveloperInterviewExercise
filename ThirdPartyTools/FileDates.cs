011d3226130f201897e6e8a33bbe342dd8dd2f9b
bpf-developer <bpf.developer@inbox.lv>
2021-07-23 13:15:57 +0530

    initial commit

---
+++ ThirdPartyTools/FileDates.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ThirdPartyTools
+{
+    public class FileDates
+    {
+        private readonly string _filePath;
+
+        public FileDates(string filePath)
+        {
+            _filePath = filePath;
+        }
+
+        public DateTimeOffset Created
+        {
+            get
+            {
+                return DateTimeOffset.UtcNow;
+            }
+        }
+
+        public DateTimeOffset Modified
+        {
+            get
+            {
+                return DateTimeOffset.UtcNow;
+            }
+        }
+    }
+}
---
+++ ThirdPartyTools/FileDetails.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ThirdPartyTools
+{
+    public class FileDetails
+    {
+        private readonly Random _random = new Random();
+
+        public string Version(string filePath)
+        {
+            return string.Format("{0}.{1}.{2}", _random.Next(5), _random.Next(8), _random.Next(22));
+        }
+
+        public int Size(string filePath)
+        {
+            return _random.Next(100000) + 1000000;
+        }
+    }
+}
