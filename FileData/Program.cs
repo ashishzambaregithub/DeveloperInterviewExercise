1acf297fe5597d4c3547771ecf4c8caf15152de4
Ashish Zambare <Ashish.Zambare@multichoice.co.za>
2021-10-17 14:00:33 +0530

    Task 1 and 2 completed with SOLID and Unit Tests

--- FileData/Program.cs
+++ FileData/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using ThirdPartyTools;
 
@@ -9,9 +10,83 @@ namespace FileData
     {
         public static void Main(string[] args)
         {
-            string strVersion = args[0];
-            string strSize = args[1];
+            if (args != null && args.Length == 2)
+            {
+                try
+                {
+                    string command = args[0];
+                    string filePath = args[1];
 
+                    #region Task 1
+                    Print.ConsolePrintMessage("Task 1 Output =>");
+                    Print.ConsolePrintMessage("File Version is " + FileVersion.GetDetails(command, filePath));
+                    #endregion
+
+                    #region Task 2 - New Business Requirements
+                    Print.ConsolePrintMessage("Task 2 Output =>");
+
+                    // Configured first arguments in AppConfig file because of if any new customization occures then just need to update the config, No need to touch the host code.
+                    string[] arrVersion = ConfigurationManager.AppSettings["VersionAgrs"].Split(',');
+                    string[] arrSize = ConfigurationManager.AppSettings["SizeAgrs"].Split(',');
+
+                    if (arrVersion.Contains(command))
+                    {
+                        Print.ConsolePrintMessage("File Version is " + FileVersion.GetDetails(command, filePath));
+                    }
+                    else if (arrSize.Contains(command))
+                    {
+                        Print.ConsolePrintMessage("File Size is " + FileSize.GetDetails(command, filePath));
+                    }
+                    else
+                    {
+                        //UnConfigured parameter received. Show proper message to the user.
+                        Print.ConsolePrintMessage("Invalid Arguments Received.");
+                    }
+                    Console.ReadLine();
+                    #endregion
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+            }
+            else
+            {
+                //Unsupported parameters received. Show proper message to the user.
+                Print.ConsolePrintMessage("Invalid Arguments Received.");
+            }
         }
     }
+
+
+    #region SOLID-SRP 
+    class FileVersion
+    {
+        internal static string GetDetails(string command, string filePath)
+        {
+            FileDetails fileDetails = new FileDetails();
+            return fileDetails.Version(filePath);
+        }
+    }
+
+    class FileSize
+    {
+        internal static int GetDetails(string command, string filePath)
+        {
+            FileDetails fileDetails = new FileDetails();
+            return fileDetails.Size(filePath);
+        }
+    }
+
+    class Print
+    {
+        internal static void ConsolePrintMessage(object message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+
+    #endregion
+
+
 }
---
+++ FileData/UnitTests.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileData
+{
+    [TestFixture]
+    public class UnitTests
+    {
+        [TestCase("Invalid Arguments")]
+        [Test]
+        void Invliad_Command_Line_Arguments_Provide()
+        {
+            FileVersion fs = new FileVersion();
+            string[] args = { "-vdaddw", "c:\test.txt" };
+            Program.Main(args);
+            Assert.IsNull(true);
+        }
+    }
+}
